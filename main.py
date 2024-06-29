7448bcf1315d47e08cd15c6f304a449c4357c05d
DanielGiansante <daniel.giansantev@gmail.com>
2024-06-28 21:18:42 -0300

    terceiro commit

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from flask import Flask, jsonify
 
 app = Flask(__name__)
+@app.route('/')
+def index():
+    return jsonify({'estado':"SP",
+                    'profissao':"programador",
+                    'idade': 26,
+                     })
 
 @app.route('/<string:nome>/', methods=['GET'])
 def get_me(nome):
