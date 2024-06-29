f7f538812484d3de674ff133000147aa70373c5b
DanielGiansante <daniel.giansantev@gmail.com>
2024-06-28 21:46:59 -0300

    agora vaii

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return render_template('index.html')
+
+@app.route('/<string:nome>', methods=['GET'])
+def get_me(nome):
+    return jsonify({'nome': nome})
 
 if __name__ == '__main__':
   app.run(port=5000)
