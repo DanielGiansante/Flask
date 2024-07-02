0e67b6fb873412523bf5fa14efde8df641bf43a3
DanielGiansante <daniel.giansantev@gmail.com>
2024-07-02 17:59:39 -0300

    pagina sobre

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
+@app.route('/sobre')
+def sobre():
+  return render_template('sobre.html')
+
 @app.route('/<string:nome>', methods=['GET'])
 def get_me(nome):
     return jsonify({'nome': nome})
